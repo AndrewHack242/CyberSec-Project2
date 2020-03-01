aa5ba4df1530a0087a4115ac88c99e9558a4d75b
Andrew Hack <andrew.hack242@gmail.com>
2020-03-01 15:27:16 -0700

    add_user.py completed, verify_hash in server written, but unable to check yet if functional

--- Server/add_user.py
+++ Server/add_user.py
@@ -6,12 +6,28 @@
     The solution contains the same number of lines (plus imports)
 """
 
+import hashlib
+import string
+from random import randint
+import random
+
+#print(hashlib.algorithms_available)
+#print(hashlib.algorithms_guaranteed)
+
+p = hashlib.sha3_512()
+
 user = input("Enter a username: ")
 password = input("Enter a password: ")
 
-# TODO: Create a salt and hash the password
-# salt = ???
-# hashed_password = ???
+# TODO: Create a salt and hash the password DONE
+letters = string.ascii_lowercase + string.ascii_uppercase + string.digits
+salt = ""
+n = randint(4,7)
+for i in range(0,n):
+    salt = salt + random.choice(letters)
+password = password + salt
+p.update(password.encode())
+hashed_password = p.digest()
 
 try:
     reading = open("passfile.txt", 'r')
